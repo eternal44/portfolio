9ecfdf942ca73dd855756ab2bd9636481283e099
James Youn <jamesyoun710@gmail.com>
2015-09-20 11:42:48 -0700

    Pre-emptively fix heroku precompiled assets error

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Portfolio
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Fix heroku error: 'precompiled assets failed'
+    config.assets.initialize_on_precompile = false
   end
 end
